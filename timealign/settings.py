6e6f1355c6a3825d789e69793af476380142148e
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-01-09 15:24:17 +0100

    Removing the database from the repo

--- timealign/settings.py
+++ timealign/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'timealign.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
     }
 }
 
