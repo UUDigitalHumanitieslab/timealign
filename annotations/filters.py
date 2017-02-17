3621fdd4b127225086d453210ef960f36191c38c
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-02-17 21:55:39 +0100

    Added label for CharFilter

--- annotations/filters.py
+++ annotations/filters.py
@@ -4,7 +4,8 @@ from .models import Annotation
 
 
 class AnnotationFilter(FilterSet):
-    word_in_source = CharFilter(name='alignment__original_fragment__sentence__word__word',
+    word_in_source = CharFilter(label='Word in source',
+                                name='alignment__original_fragment__sentence__word__word',
                                 lookup_expr='iexact',
                                 help_text='Use this to filter for words in the source text (case-insensitive)')
 
