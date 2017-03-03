b57e5d74bed946da7d1248d4b791e3944de6916f
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-03-03 19:49:24 +0100

    Fixing the tests

--- annotations/tests.py
+++ annotations/tests.py
@@ -1,20 +1,24 @@
 from django.test import TestCase
 
-from .models import Corpus, Document, Fragment, Sentence, Word, Alignment, Annotation
+from .models import Language, Corpus, Document, Fragment, Sentence, Word, Alignment
 
 
 class FragmentTestCase(TestCase):
+    fixtures = ['languages']
+
     def setUp(self):
         c = Corpus.objects.create(title='test')
         d = Document.objects.create(title='test', corpus=c)
+        en = Language.objects.get(iso='en')
+        nl = Language.objects.get(iso='nl')
 
-        self.f_en = Fragment.objects.create(language='en', document=d)
+        self.f_en = Fragment.objects.create(language=en, document=d)
         s = Sentence.objects.create(xml_id='en1', fragment=self.f_en)
         for n, w in enumerate('This has always been hard to test'.split()):
             is_target = n in [1, 3]
             Word.objects.create(word=w, is_target=is_target, sentence=s)
 
-        self.f_nl = Fragment.objects.create(language='nl', document=d)
+        self.f_nl = Fragment.objects.create(language=nl, document=d)
         s = Sentence.objects.create(xml_id='nl1', fragment=self.f_nl)
         for n, w in enumerate('Dit is altijd moeilijk te testen geweest'.split()):
             is_target = n in [1, 6]
