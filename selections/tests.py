d3c9a675d119ddf214c47bae73307c82fde57a59
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-03-06 09:53:52 +0100

    Added some more tests

--- selections/tests.py
+++ selections/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from annotations.models import Language, Corpus, Document, Sentence, Word
 
 from .models import PreProcessFragment, Selection
-from .utils import get_random_fragment
+from .utils import get_random_fragment, get_open_fragments
 
 
 class VPSelectTestCase(TestCase):
@@ -13,7 +13,7 @@ class VPSelectTestCase(TestCase):
     def setUp(self):
         c = Corpus.objects.create(title='test')
         d = Document.objects.create(title='test', corpus=c)
-        en = Language.objects.get(iso='en')
+        self.en = Language.objects.get(iso='en')
 
         self.u1 = User.objects.create_user(username='test1', email='test@test.com', password='secret')
         self.u2 = User.objects.create_user(username='test2', email='test@test.com', password='secret')
@@ -21,20 +21,26 @@ class VPSelectTestCase(TestCase):
         c.annotators = User.objects.all()
         c.save()
 
-        self.f1 = PreProcessFragment.objects.create(language=en, document=d)
+        self.f1 = PreProcessFragment.objects.create(language=self.en, document=d)
         s = Sentence.objects.create(xml_id='en1', fragment=self.f1)
         for n, w in enumerate('This is hard to test'.split()):
             Word.objects.create(word=w, sentence=s)
 
-        self.f2 = PreProcessFragment.objects.create(language=en, document=d)
+        self.f2 = PreProcessFragment.objects.create(language=self.en, document=d)
         s = Sentence.objects.create(xml_id='en2', fragment=self.f2)
         for n, w in enumerate('" Mary has just played a great move " , said John'.split()):
             Word.objects.create(word=w, sentence=s)
 
-    def test_target_words(self):
+    def test_open_fragments(self):
+        self.assertEquals(2, get_open_fragments(self.u1, self.en).count())
+        self.assertEquals(2, get_open_fragments(self.u2, self.en).count())
+
         s = Selection.objects.create(fragment=self.f1, selected_by=self.u1)
         s.words = Word.objects.filter(sentence__fragment=self.f1, word='is')
         s.save()
 
-        f = get_random_fragment(self.u1, Language.objects.get(iso='en'))
-        self.assertEquals(f, self.f2)
+        self.assertEquals(1, get_open_fragments(self.u1, self.en).count())
+        self.assertEquals(2, get_open_fragments(self.u2, self.en).count())
+
+        f = get_random_fragment(self.u1, self.en)
+        self.assertEquals(self.f2, f)
--- selections/utils.py
+++ selections/utils.py
@@ -5,13 +5,22 @@ from .models import PreProcessFragment
 
 def get_random_fragment(user, language):
     """
-    Retrieves a random PreProcessFragment from the database.
+    Retrieves a random, open PreProcessFragment from the database.
     :param user: The current User
-    :param language: The current language
+    :param language: The current Language
+    :return: A random PreProcessFragment object
+    """
+    return get_open_fragments(user, language).order_by('?').first()
+
+
+def get_open_fragments(user, language):
+    """
+    Retrieves the open PreProcessFragments for this User and Language from the database.
+    :param user: The current User
+    :param language: The current Language
     :return: A random PreProcessFragment object
     """
     return PreProcessFragment.objects \
         .filter(language=language) \
         .filter(document__corpus__in=get_available_corpora(user)) \
-        .exclude(selection__selected_by=user) \
-        .order_by('?').first()
+        .exclude(selection__selected_by=user)
