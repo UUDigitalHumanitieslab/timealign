91b6bb7b662a8c5d2f7584a8b40e7d313633642d
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-04-28 23:02:54 +0200

    Adding Italian tenses

--- annotations/utils.py
+++ annotations/utils.py
@@ -43,13 +43,13 @@ def get_color(tense):
     :param tense: The given tense
     :return: A color from the d3 color scale
     """
-    if tense in [u'Perfekt', u'present perfect', u'pretérito perfecto compuesto', u'passé composé', u'vtt']:
+    if tense in [u'Perfekt', u'present perfect', u'pretérito perfecto compuesto', u'passé composé', u'vtt', u'passato prossimo']:
         return '#1f77b4'
     elif tense in [u'Präsens', u'simple present', u'presente', u'présent', u'ott']:
         return '#ff7f0e'
     elif tense in [u'Präteritum', u'simple past', u'pretérito perfecto simple', u'ovt']:
         return '#2ca02c'
-    elif tense in [u'Plusquamperfekt', u'past perfect', u'pretérito pluscuamperfecto', u'plus-que-parfait', u'vvt']:
+    elif tense in [u'Plusquamperfekt', u'past perfect', u'pretérito pluscuamperfecto', u'plus-que-parfait', u'vvt', u'trapassato prossimo']:
         return '#d62728'
     elif tense in [u'Futur I', u'futur antérieur']:
         return '#9467bd'
@@ -59,11 +59,11 @@ def get_color(tense):
         return '#e377c2'
     elif tense in [u'pasado reciente', u'passé récent']:
         return '#7f7f7f'
-    elif tense in [u'pretérito imperfecto', u'imparfait']:
+    elif tense in [u'pretérito imperfecto', u'imparfait', u'imperfetto']:
         return '#bcbd22'
     elif tense in [u'present participle', u'participio']:
         return '#17becf'
-    elif tense in [u'infinitief']:
+    elif tense in [u'infinitief', u'infinitif']:
         return '#aec7e8'
     else:
         return ''
