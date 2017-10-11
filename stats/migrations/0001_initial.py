9ad80916c221fb1532d8d9eecdedff32a26b93b7
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-10-11 15:34:06 +0200

    Squashed migrations

--- stats/migrations/0001_initial.py
+++ stats/migrations/0001_initial.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import django.core.validators
+import picklefield.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('annotations', '0016_fragment_tense'),
+        ('annotations', '0018_auto_20171010_1310'),
     ]
 
     operations = [
@@ -16,6 +18,15 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('mds_dimensions', models.PositiveIntegerField(default=5, help_text=b'Number of dimensions to use in Multidimensional Scaling. Should be between 2 and 5.', verbose_name=b'Number of dimensions', validators=[django.core.validators.MinValueValidator(2), django.core.validators.MaxValueValidator(5)])),
+                ('mds_model', picklefield.fields.PickledObjectField(verbose_name=b'MDS model', editable=False, blank=True)),
+                ('mds_matrix', picklefield.fields.PickledObjectField(verbose_name=b'MDS matrix', editable=False, blank=True)),
+                ('mds_fragments', picklefield.fields.PickledObjectField(verbose_name=b'MDS fragments', editable=False, blank=True)),
+                ('mds_labels', picklefield.fields.PickledObjectField(verbose_name=b'MDS labels', editable=False, blank=True)),
+                ('last_run', models.DateTimeField(null=True, blank=True)),
+                ('corpus', models.ForeignKey(to='annotations.Corpus')),
+                ('documents', models.ManyToManyField(to='annotations.Document', blank=True)),
             ],
         ),
         migrations.CreateModel(
@@ -24,8 +35,11 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('as_from', models.BooleanField()),
                 ('as_to', models.BooleanField()),
+                ('use_other_label', models.BooleanField(default=False)),
+                ('other_labels', models.CharField(max_length=200, verbose_name=b'Allowed labels, comma-separated', blank=True)),
                 ('language', models.ForeignKey(to='annotations.Language')),
                 ('scenario', models.ForeignKey(to='stats.Scenario')),
+                ('tenses', models.ManyToManyField(to='annotations.Tense', blank=True)),
             ],
         ),
     ]
