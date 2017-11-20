7b5afbf0e026609b57f568ac87b6ebc4201340a0
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-11-20 09:41:23 +0100

    Allowing to enter the other_label instead of the Tense

--- annotations/management/commands/add_fragments.py
+++ annotations/management/commands/add_fragments.py
@@ -5,7 +5,7 @@ from lxml import etree
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 
-from annotations.models import Language, Corpus, Document, Fragment, Sentence, Word, Alignment
+from annotations.models import Language, Tense, Corpus, Document, Fragment, Sentence, Word, Alignment
 
 
 class Command(BaseCommand):
@@ -15,6 +15,7 @@ class Command(BaseCommand):
         parser.add_argument('corpus', type=str)
         parser.add_argument('filenames', type=str, nargs='+')
 
+        parser.add_argument('--use_other_label', action='store_true', dest='use_other_label', default=False)
         parser.add_argument('--delete', action='store_true', dest='delete', default=False,
                             help='Delete existing Fragments (and contents) for this Corpus')
 
@@ -48,10 +49,19 @@ class Command(BaseCommand):
                         doc, _ = Document.objects.get_or_create(corpus=corpus, title=row[0])
 
                         from_fragment = Fragment.objects.create(language=language_from,
-                                                                document=doc,
-                                                                tense=row[2])
+                                                                document=doc)
+
+                        # Add other_label or Tense to Fragment
+                        if options['use_other_label']:
+                            from_fragment.other_label = row[1]
+                        else:
+                            from_fragment.tense = Tense.objects.get(language=language_from, title=row[1])
+                        from_fragment.save()
+
+                        # Add Sentences to Fragment
                         add_sentences(from_fragment, row[4], row[3].split(' '))
 
+                        # Create the Fragments in other Languages and add the Alignment object
                         for m, language_to in languages_to.items():
                             if row[m]:
                                 to_fragment = Fragment.objects.create(language=language_to,
