e3352b4fd8ea2dad9d771c53d43736ebbbabc61f
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2018-03-14 14:41:34 +0100

    Added missing migration

---
+++ annotations/migrations/0023_auto_20180314_1340.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('annotations', '0022_auto_20180220_0941'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='document',
+            name='corpus',
+            field=models.ForeignKey(related_name='documents', to='annotations.Corpus'),
+        ),
+    ]
