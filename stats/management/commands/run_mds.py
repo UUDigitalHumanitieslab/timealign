c4b3adf5bb7de95457edb74a26cbff65e88e764a
bbonf <bonfil@gmail.com>
2018-04-17 10:20:46 -0700

    added management command for running mds

---
+++ stats/management/commands/run_mds.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from stats.models import Scenario
+from stats.utils import run_mds
+
+
+class Command(BaseCommand):
+    help = 'Runs MDS for scenario'
+
+    def add_arguments(self, parser):
+        parser.add_argument('scenario', type=str)
+
+    def handle(self, *args, **options):
+        # Retrieve the Scenario from the database
+        try:
+            scenario = Scenario.objects.get(title=options['scenario'])
+        except Scenario.DoesNotExist:
+            raise CommandError('Scenario with title {} does not exist'.format(
+                options['scenario']))
+
+        run_mds(scenario)
