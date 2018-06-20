e4cd534616cfdf73fde21a38c503944a92b6ab01
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2018-06-20 14:49:49 +0200

    Using the generic views

--- timealign/urls.py
+++ timealign/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.flatpages import views
 
 urlpatterns = [
-    url(r'^accounts/login/$', login, name='login'),
-    url(r'^accounts/logout/$', logout, name='logout'),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(), name='logout'),
 
     url(r'^timealign/', include('annotations.urls', namespace='annotations')),
     url(r'^preselect/', include('selections.urls', namespace='selections')),
