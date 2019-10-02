0afa2f565f0fd0fd024d6098b8fb407c7d394f49
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2019-10-02 14:39:37 +0200

    Added User actions to grant and revoke staff status

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class MyUserAdmin(UserAdmin):
-    actions = ['activate', 'deactivate']
+    actions = ['activate', 'deactivate', 'grant_staff', 'revoke_staff']
 
     def activate(self, request, queryset):
         rows_updated = queryset.update(is_active=True)
@@ -15,6 +15,16 @@ class MyUserAdmin(UserAdmin):
         rows_updated = queryset.update(is_active=False)
         self.message_user(request, '{} user(s) successfully deactivated.'.format(rows_updated))
     deactivate.short_description = 'Deactivate selected users'
+
+    def grant_staff(self, request, queryset):
+        rows_updated = queryset.update(is_staff=True)
+        self.message_user(request, 'Granted staff status to {} user(s) successfully.'.format(rows_updated))
+    grant_staff.short_description = 'Grant staff status to selected users'
+
+    def revoke_staff(self, request, queryset):
+        rows_updated = queryset.update(is_staff=False)
+        self.message_user(request, 'Revoked staff status of {} user(s) successfully .'.format(rows_updated))
+    revoke_staff.short_description = 'Revoke staff status of selected users'
 
 
 # Re-register UserAdmin
