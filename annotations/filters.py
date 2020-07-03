b044ae07cba864f41dfc67023999940fdaa6ece0
Ben Bonfil <bonfil@gmail.com>
2020-07-03 13:35:26 +0200

    changed filter labels, added headers

--- annotations/filters.py
+++ annotations/filters.py
@@ -11,7 +11,7 @@ class AnnotationFilter(FilterSet):
                                 field_name='alignment__original_fragment__sentence__word__word',
                                 lookup_expr='iexact',
                                 help_text='Use this to filter for words in the source text (case-insensitive)')
-    original_tense = ModelChoiceFilter(label='Original Tense',
+    original_tense = ModelChoiceFilter(label='Tense',
                                        field_name='alignment__original_fragment__tense',
                                        queryset=Tense.objects.all())
     labels = MultipleChoiceFilter(choices=Label.objects.all())
@@ -27,15 +27,14 @@ class AnnotationFilter(FilterSet):
 
     class Meta:
         model = Annotation
-        fields = ['corpus', 'is_no_target', 'is_translation', 'original_tense', 'tense',
-                  'original_labels', 'labels', 'word_in_source', 'annotated_by']
+        fields = ['corpus', 'is_no_target', 'is_translation', 'original_tense', 'original_labels',
+                  'tense', 'labels', 'word_in_source', 'annotated_by']
 
     def _labels_to_choices(self, queryset):
         return [(label['pk'], '{}:{}'.format(label['key__title'], label['title'])) for label in queryset.values('pk', 'key__title', 'title')]
 
     def __init__(self, l1, l2, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        request = kwargs['request']
         self.filters['original_tense'].queryset = Tense.objects.filter(language__iso=l1)
         self.filters['tense'].queryset = Tense.objects.filter(language__iso=l2)
         # match labels in the relevant languages, or labels that are not language specific
@@ -45,7 +44,7 @@ class AnnotationFilter(FilterSet):
             l1_labels_queryset = l1_labels_queryset.filter(key__corpora=kwargs['data']['corpus'])
             l2_labels_queryset = l2_labels_queryset.filter(key__corpora=kwargs['data']['corpus'])
 
-        self.filters['original_labels'] = MultipleChoiceFilter(label='Original Labels',
+        self.filters['original_labels'] = MultipleChoiceFilter(label='Labels',
                                                                field_name='alignment__original_fragment__labels',
                                                                choices=self._labels_to_choices(l1_labels_queryset))
         self.filters['labels'] = MultipleChoiceFilter(label='Labels',
