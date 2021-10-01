0f5521bda73172b28f6a035299475d8a2e0cad39
bram-pramono <bram.pramono@gmail.com>
2021-10-01 10:17:17 +0200

    Removed the name 'core' in the context path

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('landing', TemplateView.as_view(template_name='base/landing.html'), name='landing'),
-    path('landing/researcher', TemplateView.as_view(template_name='base/landing-researcher.html'), name='landing-researcher'),
-    path('landing/teacher', TemplateView.as_view(template_name='base/landing-teacher.html'), name='landing-teacher'),
-    path('landing/student', TemplateView.as_view(template_name='base/landing-student.html'), name='landing-student'),
+    path('', TemplateView.as_view(template_name='base/landing.html'), name='landing'),
+    path('researcher', TemplateView.as_view(template_name='base/landing-researcher.html'), name='landing-researcher'),
+    path('teacher', TemplateView.as_view(template_name='base/landing-teacher.html'), name='landing-teacher'),
+    path('student', TemplateView.as_view(template_name='base/landing-student.html'), name='landing-student'),
 ]
