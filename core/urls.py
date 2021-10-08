df08b811b05a3a9e3e866b76cb708f42df24b4d7
bram-pramono <bram.pramono@gmail.com>
2021-10-08 10:46:36 +0200

    Finished the basic landing home

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base/home.html'), name='home'),
-    path('researcher', TemplateView.as_view(template_name='base/researcher.html'), name='researcher'),
-    path('teacher', TemplateView.as_view(template_name='base/teacher.html'), name='teacher'),
-    path('student', TemplateView.as_view(template_name='base/student.html'), name='student'),
+    path('', TemplateView.as_view(template_name='landing/home.html'), name='home'),
+    path('researcher', TemplateView.as_view(template_name='landing/researcher.html'), name='researcher'),
+    path('teacher', TemplateView.as_view(template_name='landing/teacher.html'), name='teacher'),
+    path('student', TemplateView.as_view(template_name='landing/student.html'), name='student'),
 ]
